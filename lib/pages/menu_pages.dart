866287c1338be22674443e901d36898937436337
KentMT28 <149558061+KentMT28@users.noreply.github.com>
2023-11-16 22:07:41 +0700

    revisidrinksign

--- lib/pages/menu_pages.dart
+++ lib/pages/menu_pages.dart
@@ -15,198 +15,166 @@ class MenuPage extends StatefulWidget {
 }
 
 class _MenuPageState extends State<MenuPage> {
-  // signature menu
-  List signatureMenu = [
-    //Wagyu Beef Dry Aged x WWE
-    Signature(name: "Wagyu Beef Dry Aged x WWE", 
-    price: " 188.000", 
-    imagePath: "images/s_wagyu_beef_dry_aged.jpg", 
-    rating: "5.0" 
+  // Signature menu
+  List<Signature> signatureMenu = [
+    Signature(
+      name: "Wagyu Beef Dry Aged x WWE",
+      price: "188.000",
+      imagePath: "images/s_wagyu_beef_dry_aged.jpg",
+      rating: "5.0",
     ),
-
-    //Cena Rib Eye with Tartar Sauce
-    Signature(name: "Cena Rib Eye with Tartar Sauce", 
-    price: " 178.000", 
-    imagePath: "images/autan.jpg ", 
-    rating: "4.9" 
+    Signature(
+      name: "Cena Rib Eye with Tartar Sauce",
+      price: "178.000",
+      imagePath: "images/autan.jpg",
+      rating: "4.9",
     ),
-
-    //Under "T"aker Bone Steak
-    Signature(name: "Under T-aker Bone Steak", 
-    price: " 208.000", 
-    imagePath: "images/led.jpg ", 
-    rating: "4.8" 
+    Signature(
+      name: "Under T-aker Bone Steak",
+      price: "208.000",
+      imagePath: "images/led.jpg",
+      rating: "4.8",
     ),
-
-    //Shaky Shake Mysterio
-    Signature(name: "Shaky Shake Mysterio", 
-    price: " 36.000", 
-    imagePath: "images/ ", 
-    rating: "4.9" 
+    Signature(
+      name: "Shaky Shake Mysterio",
+      price: "36.000",
+      imagePath: "images/placeholder.jpg",
+      rating: "4.9",
     ),
   ];
 
-  // food menu
-  List foodMenu = [
-    // banger and mash
+  // Food menu
+  List<Food> foodMenu = [
     Food(
       name: "Banger and Mash",
-      price: " 45.000",
+      price: "45.000",
       imagePath: "images/m_banger_and_mash.jpg",
       rating: "4.5",
     ),
-
-    // burger
     Food(
       name: "Burger Cang",
-      price: " 28.000",
+      price: "28.000",
       imagePath: "images/m_burger.jpg",
       rating: "4.6",
     ),
-
-    //Caesar Salad
     Food(
       name: "Caesar Salad",
-      price: " 23.000",
+      price: "23.000",
       imagePath: "images/m_caesar_salad.jpg",
       rating: "4.6",
     ),
-
-    //Chicken cordon blue
     Food(
       name: "Chicken Cordon Blue",
-      price: " 23.000",
+      price: "23.000",
       imagePath: "images/m_chicken_cordon_blue.jpg",
       rating: "4.6",
     ),
-
-    //Chicken Parmesan
     Food(
       name: "Chicken Parmesan",
-      price: " 23.000",
+      price: "23.000",
       imagePath: "images/m_chicken_parmesan.jpg",
       rating: "4.6",
     ),
-
-    //Lasagna
     Food(
       name: "Lasagna",
-      price: " 23.000",
+      price: "23.000",
       imagePath: "images/m_lasagna.jpg",
       rating: "4.6",
     ),
-
-    //Potluck Mac and Cheese
     Food(
       name: "Potluck Mac and Cheese",
-      price: " 23.000",
+      price: "23.000",
       imagePath: "images/m_potluck_mac_and_cheese.jpg",
       rating: "4.6",
     ),
-
-    //Ratatouille
     Food(
       name: "Ratatouille",
-      price: " 23.000",
+      price: "23.000",
       imagePath: "images/m_ratatouille.jpg",
       rating: "4.6",
     ),
-
-    //Spaetzle
     Food(
       name: "Spaetzle",
-      price: " 23.000",
+      price: "23.000",
       imagePath: "images/m_spaetzle.jpg",
       rating: "4.6",
     ),
-
-    //Steak
     Food(
       name: "Steak",
-      price: " 23.000",
+      price: "23.000",
       imagePath: "images/m_steak.jpg",
       rating: "4.6",
     ),
   ];
-  //Drink Menu
-  List drinkMenu = [
-    //Avocado Juice
-    Drink(
-        name: "Avocado Juice",
-        price: " 18.000",
-        imagePath: "images/d_avocado juice.jpeg",
-        rating: "4.7"),
 
-    //Orange Juice
+  // Drink Menu
+  List<Drink> drinkMenu = [
     Drink(
-        name: "Orange Juice",
-        price: " 18.000",
-        imagePath: "images/d_orange_juice.jpg",
-        rating: "4.6"),
-
-    //Tomato Juice
+      name: "Avocado Juice",
+      price: "18.000",
+      imagePath: "images/d_avocado_juice.jpeg",
+      rating: "4.7",
+    ),
     Drink(
-        name: "Tomato Juice",
-        price: " 16.000",
-        imagePath: "images/d_tomato_juice.jpg",
-        rating: "4.5"),
-
-    //Banana Milkshake
+      name: "Orange Juice",
+      price: "18.000",
+      imagePath: "images/d_orange_juice.jpg",
+      rating: "4.6",
+    ),
     Drink(
-        name: "Banana Milkshake",
-        price: " 21.000",
-        imagePath: "images/d_banana_milkshake.jpg",
-        rating: "4.5"),
-    
-    //Chocolate Milkshake
+      name: "Tomato Juice",
+      price: "16.000",
+      imagePath: "images/d_tomato_juice.jpg",
+      rating: "4.5",
+    ),
     Drink(
-        name: "Chocolate Milkshake",
-        price: " 20.000",
-        imagePath: "images/d_chocolate_milkshake.jpg",
-        rating: "4.8"),
-
-    //Vanilla Milkshake
+      name: "Banana Milkshake",
+      price: "21.000",
+      imagePath: "images/d_banana_milkshake.jpg",
+      rating: "4.5",
+    ),
     Drink(
-        name: "Vanilla Milkshake",
-        price: " 20.000",
-        imagePath: "images/d_vanilla_milkshake.jpg",
-        rating: "4.7"),
-
-    //Strawberry Milkshake
+      name: "Chocolate Milkshake",
+      price: "20.000",
+      imagePath: "images/d_chocolate_milkshake.jpg",
+      rating: "4.8",
+    ),
     Drink(
-        name: "Strawberry Milkshake",
-        price: " 20.000",
-        imagePath: "images/d_strawberry_milkshake.jpg",
-        rating: "4.8"),
-        
-    //Classic Milk Tea 
+      name: "Vanilla Milkshake",
+      price: "20.000",
+      imagePath: "images/d_vanilla_milkshake.jpg",
+      rating: "4.7",
+    ),
     Drink(
-        name: "Classic Milk Tea",
-        price: " 23.000",
-        imagePath: "images/d_classic_milk_tea.jpg",
-        rating: "4.6"),
-
-    //Matcha Milk Tea
+      name: "Strawberry Milkshake",
+      price: "20.000",
+      imagePath: "images/d_strawberry_milkshake.jpg",
+      rating: "4.8",
+    ),
     Drink(
-        name: "Matcha Milk Tea",
-        price: " 24.000",
-        imagePath: "images/d_matcha_milk_tea.jpg",
-        rating: "4.8"),
-
-    //Taro Milk Tea
+      name: "Classic Milk Tea",
+      price: "23.000",
+      imagePath: "images/d_classic_milk_tea.jpg",
+      rating: "4.6",
+    ),
     Drink(
-        name: "Taro Milk Tea",
-        price: " 24.000",
-        imagePath: "images/d_taro_milk_tea.jpg",
-        rating: "4.5"),
-
-    //Lemon Tea
+      name: "Matcha Milk Tea",
+      price: "24.000",
+      imagePath: "images/d_matcha_milk_tea.jpg",
+      rating: "4.8",
+    ),
     Drink(
-        name: "Lemon Tea",
-        price: " 16.000",
-        imagePath: "images/d_lemon_tea.jpg",
-        rating: "4.5"),
-
+      name: "Taro Milk Tea",
+      price: "24.000",
+      imagePath: "images/d_taro_milk_tea.jpg",
+      rating: "4.5",
+    ),
+    Drink(
+      name: "Lemon Tea",
+      price: "16.000",
+      imagePath: "images/d_lemon_tea.jpg",
+      rating: "4.5",
+    ),
   ];
 
   @override
@@ -225,74 +193,73 @@ class _MenuPageState extends State<MenuPage> {
           style: TextStyle(color: Colors.grey[900]),
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // promobanner
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            margin: const EdgeInsets.symmetric(horizontal: 25),
-            padding: const EdgeInsets.all(25),
-            child: Row(
-              children: [
-                // Image next to "Get 32% Promo" text
-                Image.asset(
-                  'images/pasta.png', // Replace with the path to your image
-                  height: 50, // Adjust the height as needed
-                ),
-                const SizedBox(width: 20),
-                // Get 32% Promo text
-                Expanded(
-                  child: Text(
-                    'Get 40% Promo',
-                    style: GoogleFonts.dmSerifDisplay(
-                      fontSize: 20,
-                      color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Promo banner
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 25),
+              padding: const EdgeInsets.all(25),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'images/pasta.png',
+                    height: 50,
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: Text(
+                      'Get 40% Promo',
+                      style: GoogleFonts.dmSerifDisplay(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(width: 20),
-                // Redeem button
-                MyButton(
-                  text: "Redeem",
-                  onTap: () {},
-                ),
-              ],
+                  const SizedBox(width: 20),
+                  MyButton(
+                    text: "Redeem",
+                    onTap: () {},
+                  ),
+                ],
+              ),
             ),
-          ),
-          // image and text
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Row(
-              children: [
-                // Image
-                Image.asset(
-                  'images/beef.png',
-                  height: 100,
-                ),
-                const SizedBox(width: 20),
-                // Discount text
-                Text(
-                  'Special Offer!',
-                  style: TextStyle(fontSize: 18),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 25),
 
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: TextField(
-              decoration: InputDecoration(
+            // Special offer image and text
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'images/beef.png',
+                    height: 100,
+                  ),
+                  const SizedBox(width: 20),
+                  Text(
+                    'Special Offer!',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // Search bar
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                decoration: InputDecoration(
                   border: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.white),
                     borderRadius: BorderRadius.circular(20),
@@ -304,34 +271,115 @@ class _MenuPageState extends State<MenuPage> {
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.white),
                     borderRadius: BorderRadius.circular(20),
-                  )),
-            ),
-          ),
-          const SizedBox(height: 25),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Text(
-              "Food Menu",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[800],
-                fontSize: 18,
+                  ),
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 10),
 
-          Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: foodMenu.length,
-              itemBuilder: (context, index) => FoodTile(
-                food: foodMenu[index],
+            const SizedBox(height: 25),
+
+            // Food menu header
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Text(
+                "Food Menu",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[800],
+                  fontSize: 18,
+                ),
               ),
             ),
-          ),
-        ],
+
+            const SizedBox(height: 10),
+
+            // Food menu list
+            Container(
+              height: 300,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: foodMenu.length,
+                itemBuilder: (context, index) => FoodTile(
+                  food: foodMenu[index],
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 50),
+
+            // Signature menu header
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Text(
+                "Signature Menu",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[800],
+                  fontSize: 18,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            // Signature menu list
+            Container(
+              height: 300,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: signatureMenu.length,
+                itemBuilder: (context, index) => Container(
+                  margin: EdgeInsets.all(8),
+                  width: 150,
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                      signatureMenu[index].name,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 50),
+
+            // Drink menu header
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Text(
+                "Drink Menu",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[800],
+                  fontSize: 18,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            // Drink menu list
+            Container(
+              height: 300,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: drinkMenu.length,
+                itemBuilder: (context, index) => Container(
+                  margin: EdgeInsets.all(8),
+                  width: 150,
+                  color: Colors.blueGrey,
+                  child: Center(
+                    child: Text(
+                      drinkMenu[index].name,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
