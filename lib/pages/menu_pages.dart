e30b91ce6f2c4f1fe114d921a2868263d3ae300c
liuscifer <aurelliusjeremiahthomas@gmail.com>
2023-12-01 22:56:04 +0700

    udah gw samain size fotonya coba di cek lagi ye udah pas blom thankyou bangers

--- lib/pages/menu_pages.dart
+++ lib/pages/menu_pages.dart
@@ -140,7 +140,7 @@ class _MenuPageState extends State<MenuPage> {
   // Drink Menu
   final List<Drink> drinkMenu = [
     Drink(
-      name: "Avocado jus",
+      name: "Avocado juice",
       price: "18.000",
       imagePath: "images/d_avocado_juice.jpg",
       rating: "4.7",
@@ -150,7 +150,7 @@ class _MenuPageState extends State<MenuPage> {
     Drink(
       name: "Orange Juice",
       price: "18.000",
-      imagePath: "images/d_orange_juice.jpeg",
+      imagePath: "images/d_orange_juice.jpg",
       rating: "4.6",
       description:
           "Start your day with a burst of happiness while single! Orange Juice is freshly squeezed to perfection, offering a sweet and tangy flavor that's as invigorating as a stroll through a citrus grove.",
